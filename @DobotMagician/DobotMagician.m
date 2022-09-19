9e48a9b98653bf134cc0abc60b1ae18256e00b61
George Sheslow <g.sheslow@utsmotorsports.com>
2022-09-19 15:21:54 +1000

    Change ply files, update class

---
+++ @DobotMagician/DobotMagician.m
@@ -0,0 +1,80 @@
+classdef DobotMagician < handle
+
+    properties(Access =public) 
+        name = 'DobotMagician';
+    end
+    properties(Access =private)
+        defaultRealQ  = [0,pi/4,pi/4,0,0];
+        model
+        useGripper = false
+        workspace = [-2 2 -2 2 -0.3 2];
+    end
+ 
+    methods (Access = public)
+%% Define robot Function  
+        function self = DobotMagician()
+            self.CreateModel();            
+            self.PlotAndColourRobot(self.name);
+            self.model.animate(self.defaultRealQ);
+        end
+
+%% Create the robot model
+        function CreateModel(self)       
+            L(1) = Link('d',0.103+0.0362,    'a',0,      'alpha',-pi/2,  'offset',0, 'qlim',[deg2rad(-135),deg2rad(135)]);
+            L(2) = Link('d',0,        'a',0.135,  'alpha',0,      'offset',-pi/2, 'qlim',[deg2rad(5),deg2rad(80)]);
+            L(3) = Link('d',0,        'a',0.147,  'alpha',0,      'offset',0, 'qlim',[deg2rad(-5),deg2rad(85)]);
+            L(4) = Link('d',0,        'a',0.06,      'alpha',pi/2,  'offset',-pi/2, 'qlim',[deg2rad(-180),deg2rad(180)]);
+            L(5) = Link('d',-0.05,      'a',0,      'alpha',0,      'offset',pi, 'qlim',[deg2rad(-85),deg2rad(85)]);
+
+            self.model = SerialLink(L,'name',self.name);
+
+        end   
+
+%% PlotAndColourRobot
+        % Given a robot index, add the glyphs (vertices and faces) and colour them in if data is available 
+        function PlotAndColourRobot(self,fileName)
+            for linkIndex = 0:self.model.n
+                if self.useGripper && linkIndex == self.model.n
+                    [ faceData, vertexData, plyData{linkIndex+1} ] = plyread([fileName,'_J',num2str(linkIndex),'Gripper.ply'],'tri'); %#ok<AGROW>
+                else
+                    [ faceData, vertexData, plyData{linkIndex+1} ] = plyread([fileName,'_J',num2str(linkIndex),'.ply'],'tri'); %#ok<AGROW>
+                end
+                self.model.faces{linkIndex+1} = faceData;
+                self.model.points{linkIndex+1} = vertexData;
+            end
+
+            % Display robot
+            self.model.plot3d(zeros(1,self.model.n),'noarrow','workspace',self.workspace);
+            if isempty(findobj(get(gca,'Children'),'Type','Light'))
+                camlight
+            end  
+            self.model.delay = 0;
+
+            % Try to correctly colour the arm (if colours are in ply file data)
+            for linkIndex = 0:self.model.n
+                handles = findobj('Tag', self.model.name);
+                h = get(handles,'UserData');
+                vertexColours = [0.5,0.5,0.5]; % Default if no colours in plyData
+                try 
+                     vertexColours = [plyData{linkIndex+1}.vertex.red ...
+                                     , plyData{linkIndex+1}.vertex.green ...
+                                     , plyData{linkIndex+1}.vertex.blue]/255;
+
+                catch ME_1
+                    disp(ME_1);
+                    display('No vertex colours in plyData');
+                    try 
+                         vertexColours = [plyData{linkIndex+1}.face.red ...
+                                     , plyData{linkIndex+1}.face.green ...
+                                     , plyData{linkIndex+1}.face.blue]/255;
+                    catch ME_1
+                        disp(ME_1);
+                        display('Also, no face colours in plyData, so using a default colour');
+                    end
+                end
+                h.link(linkIndex+1).Children.FaceVertexCData = vertexColours;
+                h.link(linkIndex+1).Children.FaceColor = 'interp';
+            end
+        end 
+    end
+end
