f20adbabffd7b632e186edffb55abb31ce8a6aeb
1maddy <maddy.patman@gmail.com>
2022-09-19 16:38:21 +1000

    Update IRB120 class, adjust ply file names

---
+++ @IRB120/IRB120.m
@@ -0,0 +1,57 @@
+classdef IRB120 < handle
+    properties
+        model;
+        workspace = [-0.4 0.4 -0.4 0.4 -0.2 1.1];
+    end
+    methods
+        function self = IRB120()
+            self.GetIRB120();
+            self.PlotAndColourRobot();
+            drawnow
+        end
+        %% GetIRB120
+        % Create and return an IRB120 robot model
+        function GetIRB120(self)
+            pause(0.001);
+
+            L(1) = Link('d',0.29,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360 360]), 'offset',0);
+            L(2) = Link('d',0,'a',0.27,'alpha',0,'qlim', deg2rad([-360 360]), 'offset',0);
+            L(3) = Link('d',0,'a',0.07,'alpha',-pi/2,'qlim', deg2rad([-360 360]), 'offset', 0);
+            L(4) = Link('d',0.302,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360 360]),'offset', 0);
+            L(5) = Link('d',0,'a',0,'alpha',-pi/2,'qlim',deg2rad([-360,360]), 'offset',0);
+            L(6) = Link('d',0.072,'a',0,'alpha',0,'qlim',deg2rad([-360,360]), 'offset', 0);
+
+            self.model = SerialLink(L,'name','IRB120');
+        end
+        %% PlotAndColourRobot
+        % Given a robot index, add the vertices and faces and colour them
+        function PlotAndColourRobot(self)
+            for linkIndex = 0:self.model.n
+                [ faceData, vertexData, plyData{linkIndex + 1} ] = plyread(['link_',num2str(linkIndex),'.ply'],'tri'); %#ok<AGROW>                
+                self.model.faces{linkIndex + 1} = faceData;
+                self.model.points{linkIndex + 1} = vertexData;
+            end
+            % Display robot
+            self.model.plot3d(zeros(1,self.model.n),'noarrow','workspace',self.workspace);
+            if isempty(findobj(get(gca,'Children'),'Type','Light'))
+                camlight
+            end  
+            self.model.delay = 0;
+
+            % Try to correctly colour the arm (if colours are in ply file data)
+            for linkIndex = 0:self.model.n
+                handles = findobj('Tag', self.model.name);
+                h = get(handles,'UserData');
+                try 
+                    h.link(linkIndex+1).Children.FaceVertexCData = [plyData{linkIndex+1}.vertex.red ...
+                                                                  , plyData{linkIndex+1}.vertex.green ...
+                                                                  , plyData{linkIndex+1}.vertex.blue]/255;
+                    h.link(linkIndex+1).Children.FaceColor = 'interp';
+                catch ME_1
+                    disp(ME_1);
+                    continue;
+                end
+            end
+        end
+    end
+end
