d6c33de5a6a4e88679ef09700154f3dd6509ea70
1maddy <maddy.patman@gmail.com>
2022-10-23 18:04:43 +1100

    Add rotation to all ellipsoids

--- collisiondetection.m
+++ collisiondetection.m
@@ -3,7 +3,7 @@ clf
 clc
 %% IRB120
 r = IRB120(transl(0,0,0));
-r.model.animate([-pi/4, pi/2, -pi/2, 0, 0, 0]);
+r.model.animate([-pi/4, pi/4, -pi/2, 0, 0, 0]);
 %%
 try delete(Link1ellipsoid); end;
 try delete(Link2ellipsoid); end;
@@ -23,41 +23,61 @@ hold on;
 
 %%
 centerPoint{1} = transl(cell2mat(modelPoints(2))*(transl(0,0.145,0))); % FIXED
-
+rotMatrix{1} = cell2mat(modelPoints(2));
 radii{1} = [0.107,0.107,0.145];
 [X,Y,Z] = ellipsoid( centerPoint{1}(1), centerPoint{1}(2), centerPoint{1}(3), radii{1}(1), radii{1}(2), radii{1}(3) );
 Link1ellipsoid = surf(X,Y,Z);
+rotationDegrees{1} = tr2rpy(rotMatrix{1},'deg')
 view(3);
 %%
 % Link 2
 
 
 centerPoint{2} = transl(cell2mat(modelPoints(3))*transl(-0.135,0,0)); %needs to be movable
-rotationMatrix1 = cell2mat(modelPoints(3));
+rotMatrix{2} = cell2mat(modelPoints(3));
 radii{2} = [0.09,0.107,0.186];
 [X,Y,Z] = ellipsoid( centerPoint{2}(1), centerPoint{2}(2), centerPoint{2}(3), radii{2}(1), radii{2}(2), radii{2}(3) );
 Link2ellipsoid = surf(X,Y,Z);
-rotation = tr2rpy(rotationMatrix1,'deg')
+rotationDegrees{2} = tr2rpy(rotMatrix{2},'deg')
 %%
-rotate(Link2ellipsoid,[0 1 0], rotation(1), centerPoint{2})
-rotate(Link2ellipsoid,[0 0 1], -rotation(2), centerPoint{2})
-X2 = get(Link1ellipsoid,'xdata');
-x2min = min(X2)
+rotate(Link2ellipsoid,[1 0 0], rotationDegrees{2}(1), centerPoint{2})
+%%
+rotate(Link2ellipsoid,[0 1 0], rotationDegrees{2}(2), centerPoint{2})
+%%
+rotate(Link2ellipsoid,[0 0 1], rotationDegrees{2}(3), centerPoint{2})
+
 
 %%
 % Link 3/4
 centerPoint{3} = transl(cell2mat(modelPoints(4))*transl(0,0,0.142)); %needs to be movable[0.151,0,0.639]
-
+rotMatrix{3} = cell2mat(modelPoints(4));
 radii{3} = [0.231,0.09,0.07];
 [X,Y,Z] = ellipsoid( centerPoint{3}(1), centerPoint{3}(2), centerPoint{3}(3), radii{3}(1), radii{3}(2), radii{3}(3) );
 Link3ellipsoid = surf(X,Y,Z);
+rotationDegrees{3} = tr2rpy(rotMatrix{3},'deg')
+%%
+rotate(Link3ellipsoid,[1 0 0], rotationDegrees{3}(1), centerPoint{3})
+%%
+rotate(Link3ellipsoid,[0 1 0], rotationDegrees{3}(2), centerPoint{3})
+%%
+rotate(Link3ellipsoid,[0 0 1], rotationDegrees{3}(3), centerPoint{3})
 
 %%
 % Link 5/6
 centerPoint{4} = transl(cell2mat(modelPoints(6))*transl(0,0,-0.08)); %%[0.305,0,0.639];
+rotMatrix{4} = cell2mat(modelPoints(6));
 radii{4} = [0.041,0.041,0.1475];
 [X,Y,Z] = ellipsoid( centerPoint{4}(1), centerPoint{4}(2), centerPoint{4}(3), radii{4}(1), radii{4}(2), radii{4}(3) );
 Link5ellipsoid = surf(X,Y,Z);
+rotationDegrees{4} = tr2rpy(rotMatrix{4},'deg')
+%%
+rotate(Link5ellipsoid,[1 0 0], rotationDegrees{4}(1), centerPoint{4})
+%%
+rotate(Link5ellipsoid,[0 1 0], rotationDegrees{4}(2), centerPoint{4})
+%%
+rotate(Link5ellipsoid,[0 0 1], rotationDegrees{4}(3), centerPoint{4})
+
+%%
 axis equal
 
 
