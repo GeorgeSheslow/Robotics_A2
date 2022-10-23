c4acdbf762a3888ea649541f057620ca12cc3695
1maddy <maddy.patman@gmail.com>
2022-10-23 23:55:37 +1100

    Update IRB radii, update cube position

--- testCollision.m
+++ testCollision.m
@@ -7,21 +7,15 @@ set(figure,'units','normalized','outerpos',[0 0 1 1.2]);
 
 robot = IRB120(transl(0,0,0));
 
-q0 = robot.model.getpos();
-jointTransforms = {};
-modelPoints = {robot.model.base};
-for i=1:6
-    jointTransforms{i} = robot.model.A(i, q0);
-    modelPoints{i+1} = cell2mat(modelPoints(i))*cell2mat(jointTransforms(i));
-end
+    q0 = robot.model.getpos();
+        jointTransforms = {};
+        modelPoints = {robot.model.base};
+        for i=1:6
+            jointTransforms{i} = robot.model.A(i, q0);
+            modelPoints{i+1} = cell2mat(modelPoints(i))*cell2mat(jointTransforms(i));
+        end
 
-% centerPoints = [(transl(cell2mat(modelPoints(1))*(transl(0,0.0935,0))))';
-%                 (transl(cell2mat(modelPoints(2))*(transl(0,0,0.0515))))';
-%                 (transl(cell2mat(modelPoints(3))*(transl(0,0,0))))';
-%                 (transl(cell2mat(modelPoints(4))*(transl(0,0,0))))';
-%                 (transl(cell2mat(modelPoints(5))*(transl(0,0,0))))';
-%                 (transl(cell2mat(modelPoints(6))*(transl(0,0,0))))';
-%                 (transl(cell2mat(modelPoints(7))*(transl(0,0,0))))'];
+
 centerPoints = [0.0, 0.0, 0.0935;
                 0.0, 0.0515, 0.0;
                 -0.186, 0.0, 0.0;
@@ -35,15 +29,15 @@ radii = [0.09, 0.09, 0.0935; %[X,Y,Z]
          0.09, 0.07, 0.1115; %[Z,Y,X]
          0.06, 0.084, 0.07; %[Z,X,Y]
          0.06, 0.06 0.07; %[X,Y,Z]
-         0.1115, 0.441, 0.041;]; %[Z,X,Y]
+         0.1115, 0.041, 0.041;]; %[Z,X,Y]
      
- cubePosition = [0,0,0];
+ cubePosition = [0.3,-0.4,0.1];
  
  collision = CollisionChecker(robot, centerPoints, radii);
  collision.plotEllipsoids();
  hold on;
  cube = Cube(0.1,20,cubePosition);
- 
+
  collision.checkCollision(cube.getPoints)
  
  %% Dobot Collision checking with cube
