d196ce6d32686f82d023f3a6370d35789ef446c6
George Sheslow <g.sheslow@utsmotorsports.com>
2022-10-23 23:56:02 +1100

    Add testing file

--- @visualServo/visualServo.m
+++ @visualServo/visualServo.m
@@ -1,12 +1,3 @@
-
-% rob1 = DobotMagician();
-% rob1.model.base = transl(1,0,0)*trotz(pi);
-% rob2 = IRB120();
-% q0 = [0;-pi/2;0;0;pi;0];
-% rob1.model.animate([0,pi/4,pi/4,pi/2]);
-% visualServo(rob1, rob2, q0)
- %
-%%
 classdef visualServo < handle
     properties (Access = public)
         r1
