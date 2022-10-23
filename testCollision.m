d1bafda0fb77920d1b8201a88eb056b86d7c31f6
1maddy <maddy.patman@gmail.com>
2022-10-23 23:36:33 +1100

    Updating IRB ellipsoid sizes

--- testCollision.m
+++ testCollision.m
@@ -7,21 +7,35 @@ set(figure,'units','normalized','outerpos',[0 0 1 1.2]);
 
 robot = IRB120(transl(0,0,0));
 
-centerPoints = [0.0, 0.0, 0.0;
+q0 = robot.model.getpos();
+jointTransforms = {};
+modelPoints = {robot.model.base};
+for i=1:6
+    jointTransforms{i} = robot.model.A(i, q0);
+    modelPoints{i+1} = cell2mat(modelPoints(i))*cell2mat(jointTransforms(i));
+end
+
+% centerPoints = [(transl(cell2mat(modelPoints(1))*(transl(0,0.0935,0))))';
+%                 (transl(cell2mat(modelPoints(2))*(transl(0,0,0.0515))))';
+%                 (transl(cell2mat(modelPoints(3))*(transl(0,0,0))))';
+%                 (transl(cell2mat(modelPoints(4))*(transl(0,0,0))))';
+%                 (transl(cell2mat(modelPoints(5))*(transl(0,0,0))))';
+%                 (transl(cell2mat(modelPoints(6))*(transl(0,0,0))))';
+%                 (transl(cell2mat(modelPoints(7))*(transl(0,0,0))))'];
+centerPoints = [0.0, 0.0, 0.0935;
+                0.0, 0.0515, 0.0;
+                -0.186, 0.0, 0.0;
+                0.0, 0.0, 0.0225;
+                0.0, 0.095, 0.0;
                 0.0, 0.0, 0.0;
-                0.0, 0.0, 0.0;
-                0.0, 0.0, 0.0;
-                0.0, 0.0, 0.0;
-                0.0, 0.0, 0.0;
-                0.0, 0.0, 0.0;];
-            
-radii = [0.2,0.2,0.2;
-         0.2,0.2,0.2;
-         0.2,0.2,0.2;
-         0.2,0.2,0.2;
-         0.2,0.2,0.2;
-         0.2,0.2,0.2;
-         0.2,0.2,0.2;];
+                -0.1115, 0.0, 0.0;];
+radii = [0.09, 0.09, 0.0935; %[X,Y,Z]
+         0.09, 0.0515, 0.09; %[X,Z,Y]
+         0.186, 0.09, 0.107; %[Z,X,Y]
+         0.09, 0.07, 0.1115; %[Z,Y,X]
+         0.06, 0.084, 0.07; %[Z,X,Y]
+         0.06, 0.06 0.07; %[X,Y,Z]
+         0.1115, 0.441, 0.041;]; %[Z,X,Y]
      
  cubePosition = [0,0,0];
  
